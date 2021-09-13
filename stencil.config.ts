0e0d30030bbaaef0580fb3f420181630e3c4d436
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-09-13 17:49:40 +0200

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'loloof64-chessboard-stencil',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
