2e74226562f563fc9c44593a253237a364cab918
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-09-13 19:58:40 +0200

    Added coordinates and cells.

--- src/components/loloof64-chessboard-stencil/loloof64-chessboard-stencil.tsx
+++ src/components/loloof64-chessboard-stencil/loloof64-chessboard-stencil.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h, Fragment } from '@stencil/core';
 
 @Component({
   tag: 'loloof64-chessboard-stencil',
@@ -6,13 +6,56 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class Loloof64ChessboardStencil {
-
   render() {
     return (
-      <Host>
-        <div id="lower_layer"></div>
-      </Host>
+      <Fragment>
+        <div id="lower_layer">
+          <div></div>
+          {[0, 1, 2, 3, 4, 5, 6, 7].map(colIndex => {
+            const key = 'top_coord_' + colIndex;
+            const letter = String.fromCharCode('A'.charCodeAt(0) + colIndex);
+            return (
+              <p class="coordinate" key={key}>
+                {letter}
+              </p>
+            );
+          })}
+          <div></div>
+
+          {[0, 1, 2, 3, 4, 5, 6, 7].map(rowIndex => {
+            const digit = String.fromCharCode('8'.charCodeAt(0) - rowIndex);
+            return (
+              <Fragment>
+                <p class="coordinate" key={'left_coord_' + rowIndex}>
+                  {digit}
+                </p>
+                {[
+                  ...[0, 1, 2, 3, 4, 5, 6, 7].map(colIndex => {
+                    const isWhiteCell = (colIndex + rowIndex) % 2 == 0;
+                    const classes = isWhiteCell ? 'cell--white' : 'cell--black';
+                    return <div class={classes} key={'cell_' + rowIndex + colIndex}></div>;
+                  }),
+                ]}
+                <p class="coordinate" key={'right_coord_' + rowIndex}>
+                  {digit}
+                </p>
+              </Fragment>
+            );
+          })}
+
+          <div></div>
+          {[0, 1, 2, 3, 4, 5, 6, 7].map(colIndex => {
+            const key = 'bottom_coord_' + colIndex;
+            const letter = String.fromCharCode('A'.charCodeAt(0) + colIndex);
+            return (
+              <p class="coordinate" key={key}>
+                {letter}
+              </p>
+            );
+          })}
+          <div></div>
+        </div>
+      </Fragment>
     );
   }
-
 }
